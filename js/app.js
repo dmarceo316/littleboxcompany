7559c689994e0c604967cc7eb44be268e10d8a18
dmarceo316 <arceo.adam@gmail.com>
2020-01-28 19:13:44 -0800

    Fixes for nav bar

--- js/app.js
+++ js/app.js
@@ -1,30 +1,14 @@
 //navigation slider
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.shop-nav');
-    const navLinks = document.querySelectorAll('.shop-nav li');
-    
-
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-
-         //Animate Links
-    navLinks.forEach((link, index) => {
-        if(link.style.animation){
-            link.style.animation = ''
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.0}s`;
-        }
-    });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-
-    });
-   
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = "250px";
+    document.getElementById('main').style.marginLeft = "250px";
 }
 
-navSlide();
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = "0px";
+    document.getElementById('main').style.marginLeft = "0px";
+}
+
 
 const faders = document.querySelectorAll('.fade-in');
 
